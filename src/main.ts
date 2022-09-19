2158b8ad28412252b3ddbc6ebeef0627862f59a6
Egor Yakovlev <goldenyakegor@gmail.com>
2022-09-20 00:07:21 +0300

    add some code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT, () => {
+    console.log("Hello, Egor!", `Server started on port = ${PORT}`);
+  });
 }
+
 bootstrap();
+
