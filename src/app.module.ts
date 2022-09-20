d6d511b6e2057b5132078b449caff589dc92fe6e
Egor Yakovlev <goldenyakegor@gmail.com>
2022-09-20 23:24:40 +0300

    add some code

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { AlbumService } from './album/album.service';
 import { AlbumModule } from './album/album.module';
 import { TrackController } from "./track/track.controller";
 import { TrackService } from "./track/track.service";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [TrackModule, AlbumModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.dfscuay.mongodb.net/spotify-clone?retryWrites=true&w=majority'),
+    TrackModule,
+    AlbumModule],
   controllers: [AlbumController, TrackController],
   providers: [ AlbumService, TrackService],
   exports: []
