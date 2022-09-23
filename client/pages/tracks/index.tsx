4d2faebc0207e724bd5918cadb1bf6e02e1fc632
Egor Yakovlev <goldenyakegor@gmail.com>
2022-09-23 20:02:26 +0300

    add some code

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,19 +1,34 @@
 import Grid from "@mui/material/Grid";
 import React from "react";
 import MainLayout from "../../layouts/MainLayout";
-import { Button, Card } from "@mui/material";
+import { Box, Button, Card } from "@mui/material";
+import { useRouter } from "next/router";
+import { ITrack } from "../../types/track";
 
 const Index = () => {
+  const router = useRouter();
+  const tracks: ITrack[] = [
+    {_id: '1', name: 'trek 1', artist: 'some artist', text: 'some text', listens: 2, picture: ' picture.jpg', audio: 'audio.mp3', comments: []},
+    {_id: '2', name: 'trek 2', artist: 'some artist', text: 'some text', listens: 22, picture: ' picture.jpg', audio: 'audio.mp3', comments: []},
+    {_id: '3', name: 'trek 3', artist: 'some artist', text: 'some text', listens: 13, picture: ' picture.jpg', audio: 'audio.mp3', comments: []}
+  ]
+
   return (
     <MainLayout>
-      <Grid container>
-        <Card>
-          <Grid container
-                justifyContent="space-between"
-                alignItems={"center"}>
-            <h1>Список треков</h1>
-            <Button variant="contained" color="secondary">Download</Button>
-          </Grid>
+      <Grid container
+            justifyContent={"center"}>
+        <Card style={{ width: "900px" }}>
+          <Box p={3}>
+            <Grid container
+                  justifyContent={"space-between"}
+                  alignItems={"center"}>
+              <h1>Список треков</h1>
+              <Button onClick={() => router.push("/tracks/create")}
+                      variant="contained"
+                      color="secondary">Download</Button>
+            </Grid>
+          </Box>
+
         </Card>
       </Grid>
     </MainLayout>
